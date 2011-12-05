a3efe0a4020d9980054fc01651a09225c7ecbd31
Nick Lang <nick.lang@gmail.com>
2011-12-04 19:49:01 -0700

    solution for 9

--- python/euler9.py
+++ python/euler9.py
@@ -7,8 +7,44 @@
     There exists exactly one Pythagorean triplet for which a + b + c = 1000.
     Find the product abc.
 
-    a = 2*m*n
-    b = m**2 - n**2
-    c = m**2 + n**2
-"""
+    From Euclid's formula:
 
+    for m > n and m,n are Natural numbers
+    a = m^2 - n^2
+    b = 2mn
+    c = m^2 + n^2
+
+    from the constraints of our problem a + b + c = 1000
+
+    substituting the previous equations and solving for n we get:
+
+    n = (500/m) - m
+
+    using Euclids equations we can genarate Pythagorean Triples.
+    iterating over m and solving for various n's
+    we can gernate Pythagorean Triples.
+
+    The number of triples genrated this way are FAR less than by brute forcing them.
+    And it is much faster to solve.
+"""
+from utils import product_of
+m = 1
+
+
+def generate_triple(m, n, k=1):
+    a = m ** 2 - n ** 2
+    b = 2 * m * n
+    c = m ** 2 + n ** 2
+    return (k * a, k * b, k * c)
+
+
+def n(m):
+    return (500/m) - m
+
+for m in xrange(1, 1000):
+    if n(m) > 0 and m > n(m):
+        triple = generate_triple(m, n(m))
+        if sum(triple) == 1000:
+            break
+
+print product_of(*triple)
